dcbae78b207f4197dec5b231aa412600aa5bc4c6
JeremyBarrera <themaster4678@gmail.com>
2026-08-27 23:14:24 -0300

    fix: avoid Blade @error directive clash in photo zoom overlay
    Alpine's @error is parsed as Blade's @error/@enderror, causing
    ParseError: unexpected end of file, expecting elseif/else/endif.
    Use x-on:error instead so Blade does not intercept.

--- resources/views/filament/pages/partials/photo-zoom-modal.blade.php
+++ resources/views/filament/pages/partials/photo-zoom-modal.blade.php
@@ -21,7 +21,7 @@
         :alt="alt"
         class="max-w-full max-h-[75vh] object-contain rounded-xl shadow-xl"
         @click.stop
-        @error="console.error('[photo-zoom] img load failed', src)"
+        x-on:error="console.error('[photo-zoom] img load failed', src)"
     >
     <button @click="show = false" class="absolute top-4 right-4 rounded-full bg-black/60 p-2 text-white hover:bg-black/80">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/></svg>
