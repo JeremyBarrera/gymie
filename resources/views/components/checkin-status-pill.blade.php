d76c3c4000f73947f5766a321f6d50496e161601
JeremyBarrera <themaster4678@gmail.com>
2026-09-05 15:01:44 -0300

    fix(checkin): server-side pill label wrap with break, wire overdue wrap flag, voseo es strings

--- resources/views/components/checkin-status-pill.blade.php
+++ resources/views/components/checkin-status-pill.blade.php
@@ -1,4 +1,8 @@
-@props(['color' => 'gray', 'label' => '', 'size' => 'xl'])
-<x-filament::badge :color="$color" :size="$size" class="px-4 py-2 text-base font-semibold !whitespace-normal break-words max-w-full w-fit h-auto text-wrap leading-snug text-left justify-start items-start flex-col [&_.fi-badge-label-ctn]:!w-full [&_.fi-badge-label-ctn]:flex [&_.fi-badge-label-ctn]:flex-col [&_.fi-badge-label-ctn]:items-start [&_.fi-badge-label-ctn]:w-fit [&_span.fi-badge-label]:!whitespace-normal [&_span.fi-badge-label]:break-words [&_span.fi-badge-label]:whitespace-normal [&_span.fi-badge-label]:text-left [&_span.fi-badge-label]:block [&_span.fi-badge-label]:w-full [&_span.fi-badge-label]:text-left">
-    {{ $label }}
+@props(['color' => 'gray', 'label' => '', 'size' => 'xl', 'wrap' => false])
+<x-filament::badge :color="$color" :size="$size" class="px-4 py-2 text-base font-semibold whitespace-normal break-words max-w-fit w-fit text-left">
+    @if($wrap)
+        {{ \Illuminate\Support\Str::beforeLast($label, ' ') }}<br>{{ \Illuminate\Support\Str::afterLast($label, ' ') }}
+    @else
+        {{ $label }}
+    @endif
 </x-filament::badge>
